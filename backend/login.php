976fb711ed03966219821fe9f86637ca3251647f
courage <couragesblog@gmail.com>
2022-11-03 10:43:17 +0100

    Initial Commit

---
+++ backend/login.php
@@ -0,0 +1,32 @@
+<?php 
+session_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$errors = [];
+
+if (empty($username)) {
+    $errors['username'] = "Please Enter a username";
+}
+
+if (empty($password)) {
+    $errors['password'] = "Please Enter a Password";
+}
+
+
+// checking if validation is successfull
+if (empty($errors)) {
+    
+    print_r([
+        "username" => $username,
+        "password" => $password,
+    ]);
+
+}else {
+    $_SESSION['errors'] = $errors;
+    $_SESSION['data'] = $_POST;
+
+    header("Location:http://rawphp.test/");
+}
+
