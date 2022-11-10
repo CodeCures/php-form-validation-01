57f8d693a5535567a5c06b2b71274af412b42fac
courage <couragesblog@gmail.com>
2022-11-10 11:49:46 +0100

    Latest update

--- backend/register.php
+++ backend/register.php
@@ -16,16 +16,8 @@ if (empty($password)) {
 if (empty($errors)) {
     try {
         $conn = new PDO("mysql:host=localhost;dbname=class_db", 'root', '');
-        // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // $sql = "CREATE TABLE users (
-        //     id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        //     username VARCHAR(255) NOT NULL,
-        //     password VARCHAR(255) NOT NULL
-        //     )";
-
-        // $conn->exec($sql);
         $hashedPs = password_hash($password, PASSWORD_DEFAULT);
         $insertSql = "
             INSERT INTO users (username, password) VALUES ('$username', '$hashedPs')
@@ -33,7 +25,6 @@ if (empty($errors)) {
 
 
         $conn->exec($insertSql);
-
         header('Location: /public');
 
       } catch(PDOException $e) {
