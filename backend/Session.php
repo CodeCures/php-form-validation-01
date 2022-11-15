a3153b8e066ec9571899eb33ddcd9d725acb86b9
courage <couragesblog@gmail.com>
2022-11-15 16:49:03 +0100

    Check if session started or key exists

--- backend/Session.php
+++ backend/Session.php
@@ -6,12 +6,27 @@ class Session
 {
     public static function put($key, $value)
     {
-        session_start();
+        if(session_status() === PHP_SESSION_NONE){
+            session_start();
+        }
+
         $_SESSION[$key] = $value;
+        $_SESSION['timeout'] = time();
     }
 
     public static function get($key)
     {
+        session_start();
         return $_SESSION[$key];
+    }
+
+    public static function flush()
+    {
+        if (isset($_SESSION['timeout'])) {
+            if (time() - $_SESSION['timeout'] > 0.5) {
+                session_unset();
+                session_destroy();
+            }
+        }
     }
 }
