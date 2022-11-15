d3d968043d462c5e06e9a9938c9b5e24d28acd37
courage <couragesblog@gmail.com>
2022-11-15 11:45:27 +0100

    Remove redundant code

--- backend/login.php
+++ backend/login.php
@@ -1,35 +1,18 @@
 <?php
 
 use Backend\Auth;
-use Backend\Session;
 
 require_once './Auth.php';
 require_once './Session.php';
 
-// create Validator class with two methods: validate and validated;
-// validate shoud validate your inpute records 
-// if wrong store the error message in a session usin our session class
-// if data is correct put them in data property as an array
-Validator::validate([
+$validator = Validator::validate([
     'username' => 'required',
     'password' => 'required'
 ]);
 
+Auth::login(...array_values($validator->validated()));
 
-// return the array here
-Validator::validated(); //[username => "courage", password => 'pass123'];
-Auth::login($username, $password);
+var_dump(Auth::user());
 
-
-// checking if validation is successfull
-// if (empty($errors)) {
-    
-//     var_dump(Auth::user());
-// } else {
-//     Session::put('errors', $errors);
-//     Session::put('data', $_POST);
-
-//     header("Location:http://rawphp.test/");
-// }
 
 
