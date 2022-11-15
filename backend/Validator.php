84d688d5ae813cc2dd8c9ba8a5b90138c1686143
courage <couragesblog@gmail.com>
2022-11-15 11:45:13 +0100

    complete validator class

--- backend/Validator.php
+++ backend/Validator.php
@@ -2,23 +2,54 @@
 
 namespace Backend;
 // assignment
-class Validator{
+class Validator
+{
 
     private static $data = [];
+    private static $errors = [];
+    private static $postData = ['username' => 'Courage', 'password' => 'pass123'];
 
     public static function validate(array $data)
     {
-        foreach($data as $key => $value){
-            if(isset($key)){
-                self::$data[$key] = $value;
-            }else{
-                Session::put('errors', "there was an error");
-            }   
+        $keys = array_keys($data);
+
+        for ($i = 0; $i < count($keys); $i++) {
+            if (
+                $data[$keys[$i]] === 'required' &&
+                isset(self::$postData[$keys[$i]]) &&
+                !empty(self::$postData[$keys[$i]])
+            ) {
+                self::$data[$keys[$i]] = self::$postData[$keys[$i]];
+            } else {
+                self::$errors[$keys[$i]] = $keys[$i] . ' is  required';
+            }
         }
+
+
+        if (count(self::$errors) > 0) {
+            self::$data = [];
+            Session::put('errors', self::$errors);
+            header("Location:http://rawphp.test/");
+        }
+
+        return new self;
     }
 
-    public static function validated()
+    public function validated()
     {
         return self::$data;
     }
+
+    public function errors()
+    {
+        return self::$errors;
+    }
 }
+
+$validator = Validator::validate([
+    'username' => 'required',
+    'password' => 'required'
+]);
+
+var_dump($validator->validated());
+var_dump($validator->errors());
