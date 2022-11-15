03dc0c7aff2808e4a2162bddd9c081c607746a9c
courage <couragesblog@gmail.com>
2022-11-15 16:48:41 +0100

    Import validator class

--- backend/login.php
+++ backend/login.php
@@ -1,16 +1,18 @@
 <?php
 
 use Backend\Auth;
+use Backend\Validator;
 
 require_once './Auth.php';
 require_once './Session.php';
+require_once './Validator.php';
 
 $validator = Validator::validate([
     'username' => 'required',
     'password' => 'required'
 ]);
 
-Auth::login(...array_values($validator->validated()));
+Auth::login(...array_values($validator->validated())); 
 
 var_dump(Auth::user());
 
